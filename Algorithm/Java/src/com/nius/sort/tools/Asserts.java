0b13bbae2abf9d61113e1eca39948abffab44a21
曾维俊 <niuszeng@163.com>
2019-10-08 23:42:42 +0800

    二分搜索算法实现

--- Algorithm/Java/src/com/nius/sort/tools/Asserts.java
+++ Algorithm/Java/src/com/nius/sort/tools/Asserts.java
@@ -2,8 +2,13 @@ package com.nius.sort.tools;
 
 public class Asserts {
 	public static void test(boolean value) {
+		test(value, null);
+	}
+
+	public static void test(boolean value, String tag) {
 		try {
 			if (!value) throw new Exception("测试未通过");
+			else if (null != tag && !tag.equals("")) System.out.println(tag + " 测试通过");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
